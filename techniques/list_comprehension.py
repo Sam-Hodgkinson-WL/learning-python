13c503fc17e1a1f69810a048efffaa45aba76a3d
Sam <sam.hodgkinson@worldline.com>
2023-04-05 16:53:41 +0100

    Added a file for list comprehension

--- techniques/list_comprehension.py
+++ techniques/list_comprehension.py
@@ -17,3 +17,25 @@ if print_list_comprehension:
     filtered_list = [item for item in number_list if item % 10 == 0]
 
     print(filtered_list)
+
+
+    ## STRING SPLIT METHOD
+
+    sample_string = 'My name is Sam, I live in Fife, Scotland, I am a geek!'
+
+    print(sample_string.split(',')) # Splits at ever comma
+
+    def clean_word(word):
+        print(word.replace(',', ' and')) # Kind of broken, but you get the idea LOL!
+
+    clean_word(sample_string)
+
+    print(sample_string.split(' '))
+
+    [clean_word(word) for word in sample_string.split(' ') if len(word) < 3]
+
+    ## Ready to blow your mind?
+    ## NESTED LIST COMPREHENSION
+
+    print('This is a new one!')
+    [[clean_word(word) for word in sentence.split()] for sentence in sample_string.split('.')]
